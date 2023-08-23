7c71c79e1cb6a4b24dded009a381bc5a5c892fec
Zane Wilson <wilsonzanea@gmail.com>
2023-08-23 11:26:50 -0400

    setting up topic router

---
+++ src/server/api/routers/topic.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+// TODO - fix TS stuff
+
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const topicRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+
+  create: protectedProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.topic.create({
+        data: {
+          title: input.title,
+          userId: ctx.session.user.id,
+        }
+      })
+    })
+});
