07a9da3ba9aecfda187636f7ac0eeab27d1ca2ad
Zane Wilson <wilsonzanea@gmail.com>
2023-08-23 17:29:06 -0400

    notecard stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import Head from "next/head";
 import { api, type RouterOutputs } from "~/utils/api";
 import { Header } from "~/components/Header";
 import { useState } from "react";
+import { NoteEditor } from "~/components/NoteEditor";
+import { NoteCard } from "~/components/NoteCard";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({
-    text: "world"
+    text: "world",
   });
 
   return (
@@ -24,7 +26,7 @@ export default function Home() {
   );
 }
 
-type Topic = RouterOutputs['topic']['getAll'][0];
+type Topic = RouterOutputs["topic"]["getAll"][0];
 
 const Content: React.FC = () => {
   const { data: sessionData } = useSession();
@@ -36,14 +38,35 @@ const Content: React.FC = () => {
       enabled: sessionData?.user !== undefined,
       onSuccess: (data) => {
         setSelectedTopic(selectedTopic ?? data[0] ?? null);
-      }
+      },
     }
   );
 
   const createTopic = api.topic.create.useMutation({
     onSuccess: () => {
       void refetchTopics(); // void is used to ignore the promise
+    },
+  });
+
+  const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
     }
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
   });
 
   return (
@@ -60,28 +83,49 @@ const Content: React.FC = () => {
                 }}
               >
                 {topic.title}
-                </a>
+              </a>
             </li>
           ))}
         </ul>
         <input
           type="text"
           placeholder="New Topic"
-          className='input-bordered input input-sm w-full'
+          className="input input-bordered input-sm w-full"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               createTopic.mutate({
-                title: e.currentTarget.value
+                title: e.currentTarget.value,
               });
               e.currentTarget.value = "";
             }
           }}
         />
       </div>
-      <div className="col-span-3"></div>
+      <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+
+        <NoteEditor
+          onSave={({ title, content }) => {
+            void createNote.mutate({
+              title,
+              content,
+              topicId: selectedTopic?.id ?? "",
+            });
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 // function AuthShowcase() {
 //   const { data: sessionData } = useSession();
